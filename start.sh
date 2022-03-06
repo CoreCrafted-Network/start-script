11b00dfd13363e657101e7dce198a00ca6106f4c
Node USW <usw@sys.corecrafted.net>
2022-03-06 02:31:13 -0800

    Initial commit

---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source ./startconf.sh
+
+export JAVA_HOME=/usr/lib/jvm/java-$JAVA_VER-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+if [ -z "$JAR_NAME" ]; then
+  FILELIST=($(ls | egrep "((waterfall)|(paper)|(spigot)).*\.jar"))
+  if [[ ${#FILELIST[@]} > 1 ]]; then
+    echo "(!) More than 1 JARs exist! Remove unused JARs or specify JAR_NAME before continue!"
+    exit 1;
+  else
+    JAR_NAME=${FILELIST[0]}
+  fi
+fi
+
+print_info(){
+  echo "====================="
+  echo Memory: $MEMORY
+  echo JAR: $JAR_NAME
+  echo Java: $JAVA_HOME
+  echo "======================"
+}
+
+start_server() {
+  java -Xmx$MEMORY $ARGS -jar $JAR_NAME;
+  read -t 10 -p "Restarting in 10 Seconds (Press ENTER to restart now / Ctrl-C to terminate)"
+  clear
+  start_server
+}
+
+print_info
+start_server
